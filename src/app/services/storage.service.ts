099a987040af581980d9f1ce6aac67b3cbf882ce
Kalvin <kalvinhart@hotmail.co.uk>
2022-10-19 17:25:14 +0100

    added storage service

---
+++ src/app/services/storage.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Todo } from '../types/Todo';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StorageService {
+  constructor() {}
+
+  get(key: string): Todo[] {
+    return JSON.parse(localStorage.getItem(key) ?? '');
+  }
+
+  save(key: string, todos: Todo[]) {
+    localStorage.setItem(key, JSON.stringify(todos));
+  }
+}
