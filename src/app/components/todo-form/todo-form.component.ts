a81ce00a5ac0ffbad0daa2faea9dc06108cd1056
Kalvin <kalvinhart@hotmail.co.uk>
2022-10-19 17:44:39 +0100

    implemented form submit method

--- src/app/components/todo-form/todo-form.component.ts
+++ src/app/components/todo-form/todo-form.component.ts
@@ -1,17 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+import { v4 as uuid } from 'uuid';
+
+import { TodoService } from 'src/app/services/todo.service';
+import { Todo } from 'src/app/types/Todo';
+
 @Component({
   selector: 'app-todo-form',
   templateUrl: './todo-form.component.html',
   styleUrls: ['./todo-form.component.css'],
 })
 export class TodoFormComponent implements OnInit {
-  constructor() {}
+  constructor(private todoService: TodoService) {}
 
   ngOnInit(): void {}
 
   onSubmit(form: NgForm): void {
-    console.log(form);
+    const textValue = form.controls['todoText'].value;
+    if (textValue === '') return;
+
+    const todo: Todo = {
+      id: uuid(),
+      text: textValue,
+      completed: false,
+    };
+
+    this.todoService.saveTodo(todo);
   }
 }
