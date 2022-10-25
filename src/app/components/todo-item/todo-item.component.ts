b465919b7286ca86b07d96c69f7fad5b5f0220cf
Kalvin <kalvinhart@hotmail.co.uk>
2022-10-25 16:33:41 +0100

    added functionality to edit a todo

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -9,12 +9,35 @@ import { Todo } from 'src/app/types/Todo';
 export class TodoItemComponent implements OnInit {
   @Input() todo!: Todo;
   @Output() handleDelete: EventEmitter<string> = new EventEmitter();
+  @Output() handleEdit: EventEmitter<Todo> = new EventEmitter();
+  editing: boolean = false;
+  editText: string = '';
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.editText = this.todo.text;
+  }
 
   deleteTodo(id: string) {
     this.handleDelete.emit(id);
+  }
+
+  toggleEdit(): void {
+    this.editing = !this.editing;
+  }
+
+  submitEdit(): void {
+    if (this.editText === '') return;
+
+    const newTodo = {
+      ...this.todo,
+      text: this.editText,
+    };
+
+    this.handleEdit.emit(newTodo);
+
+    this.editing = false;
+    this.editText = '';
   }
 }
